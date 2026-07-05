b4c41ec5e375e97beb07d584d0c86ec4831b6857
ravanan <mailravan@gmail.com>
2026-07-05 18:44:49 +0530

    api chnages

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/")
